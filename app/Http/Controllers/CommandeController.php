1f05901c22135350d6af28f7d937c3d26e928f90
ndongombathie <ndongo.mbath@univ-thies.sn>
2026-02-10 21:01:44 +0000

    chaque caissier doit voire des propres statistique qui les concerne 2

--- app/Http/Controllers/CommandeController.php
+++ app/Http/Controllers/CommandeController.php
@@ -16,7 +16,7 @@ class CommandeController extends Controller
     public function index(Request $request)
     {
         try {
-            $query = Commande::with('details', 'client', 'vendeur');
+            $query = Commande::with('details', 'client', 'vendeur')
                 ->where('vendeur_id', Auth::user()->id);
 
             if ($request->filled('date')) {
