688012e32745bc497693d9853424dcabe92fde72
ndongombathie <ndongo.mbath@univ-thies.sn>
2026-02-13 19:59:52 +0000

    ajout de HasFactory

--- app/Models/CaissierCaisseJournal.php
+++ app/Models/CaissierCaisseJournal.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CaissierCaisseJournal extends Model
 {
-    use UserFactory;
+    use HasFactory;
     use HasUuids;
 
     public $incrementing = false;
