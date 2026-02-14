32efca90f38e3c2bf914b24de3b2c003596a5da8
ndongombathie <ndongo.mbath@univ-thies.sn>
2026-02-14 00:34:48 +0000

    enregistrement versement

---
+++ database/migrations/2026_02_14_002216_create_enregistrer_versements_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('enregistrer_versements', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('caissier_id')->nullable()->foreignId('users')->cascadeOnDelete();
+            $table->bigInteger('montant');
+            $table->text('observation')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('enregistrer_versements');
+    }
+};
