262b8b2c308c3909257937a16358fa322f9147d5
ndongombathie <ndongo.mbath@univ-thies.sn>
2026-02-14 10:24:36 +0000

    enregistrer_versements

--- database/migrations/2026_02_14_002216_create_enregistrer_versements_table.php
+++ database/migrations/2026_02_14_002216_create_enregistrer_versements_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->text('observation')->nullable();
             $table->date('date');
             $table->timestamps();
-        });
+        }); 
     }
 
     /**
