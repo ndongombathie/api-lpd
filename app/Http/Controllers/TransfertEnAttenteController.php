13bd8e56c65b9dc166d5b101f7c0313727c3a112
ndongombathie <ndongo.mbath@univ-thies.sn>
2026-02-15 15:28:59 +0000

    annuler transfert

---
+++ app/Http/Controllers/TransfertEnAttenteController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\transfertEnAttente;
+use App\Http\Requests\StoretransfertEnAttenteRequest;
+use App\Http\Requests\UpdatetransfertEnAttenteRequest;
+
+class TransfertEnAttenteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoretransfertEnAttenteRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(transfertEnAttente $transfertEnAttente)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatetransfertEnAttenteRequest $request, transfertEnAttente $transfertEnAttente)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(transfertEnAttente $transfertEnAttente)
+    {
+        //
+    }
+}
