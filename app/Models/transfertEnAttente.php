e8972bf55cce73466a8f3eab2b5f5e90ec52cd52
ndongombathie <ndongo.mbath@univ-thies.sn>
2026-02-16 21:07:34 +0000

    tranfert -> transferEnAttente version stable

--- app/Models/transfertEnAttente.php
+++ app/Models/transfertEnAttente.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class transfertEnAttente extends Model
+class TransfertEnAttente extends Model
 {
     /** @use HasFactory<\Database\Factories\TransfertEnAttenteFactory> */
     use HasFactory;
