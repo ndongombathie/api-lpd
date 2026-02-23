01a1df5358b512b4beee923c9777b6e71409ac52
ndongombathie <ndongo.mbath@univ-thies.sn>
2026-02-23 13:20:23 +0000

    veroue des commandes en attente

--- app/Http/Controllers/CommandeController.php
+++ app/Http/Controllers/CommandeController.php
@@ -300,6 +300,11 @@ class CommandeController extends Controller
     public function annuler(string $id)
     {
         $commande = Commande::findOrFail($id);
+        if($commande->statut !== 'en_attente'){
+            return response()->json([
+                'message' => 'Seules les commandes en attente peuvent être annulées',
+            ], 400);
+        }
         $commande->update(['statut' => 'annulee','caissier_id'=>Auth::user()->id]);
         $commande->load('details', 'vendeur', 'client');
 
