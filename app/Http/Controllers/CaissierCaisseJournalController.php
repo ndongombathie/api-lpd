4f667fc625d430753a456cead7cd18b86133777c
ndongombathie <ndongo.mbath@univ-thies.sn>
2026-02-26 22:23:41 +0000

    caissier/caisses-journals regle du filtre

--- app/Http/Controllers/CaissierCaisseJournalController.php
+++ app/Http/Controllers/CaissierCaisseJournalController.php
@@ -41,19 +41,11 @@ class CaissierCaisseJournalController extends Controller
         try {
             $query = CaissierCaisseJournal::query()->with('caissier')->orderByDesc('date');
 
-           /* if (!$request->filled('date_debut')) {
-                $request->merge(['date_debut' => Carbon::today()->toDateString()]);
-            }
-            if (!$request->filled('date_fin')) {
-                $request->merge(['date_fin' => Carbon::today()->toDateString()]);
-            }*/
 
-            if ($request->filled('date_debut')) {
-                $query->where('date', '>=', $request->date_debut);
+            if ($request->filled('date')) {
+                $query->where('date', $request->date);
             }
-            if ($request->filled('date_fin')) {
-                $query->where('date', '<=', $request->date_fin);
-            }
+            
             # filtrer par nom et mail des caissier en utilisant search
             if ($request->filled('search')) {
                 $query->where(function ($q) use ($request) {
