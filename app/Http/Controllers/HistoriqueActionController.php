0ad367aea8c14e08008c080cdda159893573b5cb
ndongombathie <ndongo.mbath@univ-thies.sn>
2026-03-02 00:10:41 +0000

    gesiions de l'historique des actions

--- app/Http/Controllers/HistoriqueActionController.php
+++ app/Http/Controllers/HistoriqueActionController.php
@@ -14,9 +14,22 @@ class HistoriqueActionController extends Controller
     public function index()
     {
         try {
-            return HistoriqueAction::with('user','produit')
-            ->orderBy('created_at', 'desc')
-            ->paginate(10);
+            $historiqueActions = HistoriqueAction::with('user','produit')
+            ->orderBy('created_at', 'desc');
+
+            if ($request->filled('action')) {
+                $historiqueActions->where('action', $request->input('action'));
+            }
+
+            if ($request->filled('search')) {
+                    $historiqueActions->where(function ($q) use ($request) {
+                        $q->where('user.nom', 'like', '%'.$request->search.'%')
+                          ->orWhere('user.prenom', 'like', '%'.$request->search.'%')
+                          ->orWhere('produit.nom', 'like', '%'.$request->search.'%');
+                    });
+                }
+
+            return response()->json($historiqueActions->paginate(10), 200);
         } catch (\Throwable $th) {
             return response()->json(['message' => $th->getMessage()], 500);
         }
