9a924570d29bf6d5b1f69681d57480cfd31c2cff
ndongombathie <ndongo.mbath@univ-thies.sn>
2026-03-02 15:03:32 +0000

    gesiions des erreurs sur l'historiques des actions

--- app/Http/Controllers/HistoriqueActionController.php
+++ app/Http/Controllers/HistoriqueActionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\HistoriqueAction;
 use App\Http\Requests\StoreHistoriqueActionRequest;
 use App\Http\Requests\UpdateHistoriqueActionRequest;
+use Illuminate\Http\Request;
 
 class HistoriqueActionController extends Controller
 {
@@ -14,24 +15,33 @@ class HistoriqueActionController extends Controller
     public function index(Request $request)
     {
         try {
-            $historiqueActions = HistoriqueAction::with('user','produit')
-            ->orderBy('created_at', 'desc');
+            $historiqueActions = HistoriqueAction::with('user', 'produit')
+                ->latest();
 
             if ($request->filled('action')) {
-                $historiqueActions->where('action', $request->input('action'));
+                $historiqueActions->where('action', $request->action);
             }
 
             if ($request->filled('search')) {
-                 $search = $request->input('search');
-                    $historiqueActions->where(function ($q) use ($search) {
-                        $q->where('user.nom', 'like', '%'.$search.'%')
-                          ->orWhere('user.prenom', 'like', '%'.$search.'%')
-                          ->orWhere('produit.nom', 'like', '%'.$search.'%');
+                $search = $request->search;
+
+                $historiqueActions->where(function ($q) use ($search) {
+
+                    $q->whereHas('user', function ($q2) use ($search) {
+                        $q2->where('nom', 'like', "%$search%")
+                        ->orWhere('prenom', 'like', "%$search%");
+                    })
+
+                    ->orWhereHas('produit', function ($q3) use ($search) {
+                        $q3->where('nom', 'like', "%$search%");
                     });
-                }
+
+                });
+            }
 
             return response()->json($historiqueActions->paginate(10), 200);
-        } catch (\Throwable $th) {
+        }
+        catch (\Throwable $th) {
             return response()->json(['message' => $th->getMessage()], 500);
         }
     }
