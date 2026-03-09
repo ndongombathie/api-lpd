2106279d02eec2774062e85a7372b82d6bd6e69c
ndongombathie <ndongo.mbath@univ-thies.sn>
2026-03-09 18:03:04 +0000

    somme encaissement, decaissement

--- app/Http/Controllers/CaissierCaisseJournalController.php
+++ app/Http/Controllers/CaissierCaisseJournalController.php
@@ -20,19 +20,31 @@ class CaissierCaisseJournalController extends Controller
     {
         $query = CaissierCaisseJournal::query()->with('caissier')->orderByDesc('date');
 
-        if ($request->filled('date_debut')) {
+        if ($request->filled('date_debut') && $request->filled('date_fin')) {
+            $query->whereBetween('date', [$request->date_debut, $request->date_fin]);
+        }elseif ($request->filled('date_debut')) {
             $query->where('date', '>=', $request->date_debut);
-        }
-        if ($request->filled('date_fin')) {
+        }elseif ($request->filled('date_fin')) {
             $query->where('date', '<=', $request->date_fin);
         }
-        if ($request->has('cloture')) {
-            $query->where('cloture', (bool) $request->cloture);
+        # filtrer par nom et mail des caissier en utilisant search
+        if ($request->filled('search')) {
+            $query->where(function ($q) use ($request) {
+                $q->where('caissier.nom', 'like', '%'.$request->search.'%')
+                    ->orWhere('caissier.email', 'like', '%'.$request->search.'%');
+            });
         }
 
         $journals = $query->paginate(10);
 
-        return response()->json($journals);
+        return response()->json(
+             [
+                'liste_caisiers' => $journals,
+                'somme_encaissements' => $journals->sum('total_encaissements'),
+                'somme_decaissements' => $journals->sum('total_decaissements'),
+                'somme_solde' => $journals->sum('solde_reel'),
+                'nombre_de_caissier' => $journals->count(),
+             ]);
     }
 
     #toutes les caisserjounal et filter par date mettre la date par defaut a la date d'aujourd'hui
