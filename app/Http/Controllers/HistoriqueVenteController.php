1917398bb8fe3033dc2f9fdc10566e4cf9bcb2ae
ndongombathie <ndongo.mbath@univ-thies.sn>
2026-03-09 16:45:03 +0000

    ajout filtre

--- app/Http/Controllers/HistoriqueVenteController.php
+++ app/Http/Controllers/HistoriqueVenteController.php
@@ -206,6 +206,8 @@ class HistoriqueVenteController extends Controller
                 $totalVentes = HistoriqueVente::with('vendeur')
                     ->select('vendeur_id', DB::raw('COUNT(quantite) as total_ventes')
                     ,DB::raw('SUM(montant) as total_encaisses'))
+                    # date par defaut a la date d'aujourd'hui
+                    ->whereDate('created_at', date('Y-m-d'))
                     ->groupBy('vendeur_id');
             }
 
