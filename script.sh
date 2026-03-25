07511c22bbe6e6aa6e59f1ddd219966b98180fe4
ndongombathie <ndongo.mbath@univ-thies.sn>
2026-03-25 14:48:39 +0000

    docker-compose,dockerfile et nginx version final

--- script.sh
+++ script.sh
@@ -1,5 +1,14 @@
-#!/usr/bin/env zsh
-mysql -u root -p042002 <<EOF
-USE une_boutique;
-SELECT * FROM users;
-EOF
+#!/bin/bash
+
+echo "⏳ Waiting for MySQL..."
+
+while ! nc -z db 3306; do
+  sleep 2
+done
+
+echo "✅ MySQL is ready!"
+
+php artisan config:clear
+php artisan cache:clear
+
+php artisan reverb:start --host=0.0.0.0 --port=8080
