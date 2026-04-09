26c4901a9e93ce2e3806051a06e21d08f5f3a054
ndongombathie <ndongo.mbath@univ-thies.sn>
2026-04-09 16:26:53 +0000

    ajout de CI/CD

--- routes/api.php
+++ routes/api.php
@@ -183,14 +183,14 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('produits-controle-depots', [TransfertEnAttenteController::class, 'produitsControleDepots']);
     Route::get('produits-disponibles-boutique/code-barre/{code}', [TransfertEnAttenteController::class, 'getProductByCode']);
 
-    
+
     Route::get('nombre-transfert-valide', [TransfertEnAttenteController::class, 'nombreTransfertValide']);
     Route::get('nombre-transfert-en-attente', [TransfertEnAttenteController::class, 'nombreTransfertEnAttente']);
     Route::get('nombre-transfert-total', [TransfertEnAttenteController::class, 'nombreTransfertTotal']);
 
 
 
-    // ---------------- DECAISSEMENTS ----------------
+    // ---------------- DECAISSEMENTS -------------
 
     Route::get('decaissements/export', [DecaissementController::class, 'exportAll']);
     Route::get('decaissements/stats', [DecaissementController::class, 'stats']);
