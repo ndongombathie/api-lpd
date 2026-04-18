60aede574d25e3651277003ba70e58e64abfd645
ndongombathie <ndongo.mbath@univ-thies.sn>
2026-04-18 14:30:49 +0000

    = Str::random(10);

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,8 +20,8 @@ class AuthController extends Controller
             ]);
 
         $email = strtolower(trim($credentials['email']));
-        dd($email);
         $user = User::where('email_hash', hash('sha256', $email))->first();
+        dd($user);
 
         if (! $user || ! Hash::check($credentials['password'], $user->password)) {
             throw ValidationException::withMessages([
