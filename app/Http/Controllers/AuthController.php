6a1433728a2cbfeb08faaaa7603d558803fa66af
ndongombathie <ndongo.mbath@univ-thies.sn>
2026-04-18 13:51:15 +0000

    = Str::random(10);

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,7 +20,7 @@ class AuthController extends Controller
         ]);
 
 
-        dd($credentials->all());
+        dd($request->all());
         $user = User::where('email_hash', hash('sha256', $credentials['email']))->first();
         dd($user);
 
